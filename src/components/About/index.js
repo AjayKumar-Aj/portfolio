0b14d44c34f550ec86610d21b3b0725c748c18d3
ajaykumar <ajaykumar914491@gmail.com>
2018-12-18 09:49:56 +0530

    wipp

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Leftimg, Text } from './style.js';
 
 class About extends Component {
     render() {
         return (
-          <div style={{marginTop: '100px'}}> about
-          </div>
+            <div display="inline-block" width="100%">
+                <div width="300px" float="left" height="500px" >
+                    <Leftimg>
+                    <img src={require('./Insta.png')} height="200" width="200"/>
+                    </Leftimg>
+                </div>
+                <div width="300px" float="right" height="500px">
+                    <Text>Ajay</Text>
+                </div>
+            </div>
         );
     }
 }
---
+++ src/components/About/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Leftimg = styled.div`
+    margin-top: 10%;
+       
+`;
+
+
+export const Text = styled.div`
+    color: blue;
+    
+`;
