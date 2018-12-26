0a114fb5d9d1891fa31229811e25ae3810374f6a
ajaykumar <ajaykumar914491@gmail.com>
2018-12-26 08:53:48 +0530

    Updates

---
+++ precache-manifest.3fdf2883d22c9f628bde4eac9728be50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ed692d6d7adaf71e21d",
+    "url": "/portfolio/static/css/main.9f30bc59.chunk.css"
+  },
+  {
+    "revision": "3ed692d6d7adaf71e21d",
+    "url": "/portfolio/static/js/main.3ed692d6.chunk.js"
+  },
+  {
+    "revision": "2d9205c369f97971e502",
+    "url": "/portfolio/static/js/1.2d9205c3.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "03f40b67b63d9c1c1a5a792109bbc699",
+    "url": "/portfolio/static/media/Insta.03f40b67.png"
+  },
+  {
+    "revision": "8651bfc4cffc584a25efa26246c11281",
+    "url": "/portfolio/static/media/Fb.8651bfc4.png"
+  },
+  {
+    "revision": "ae91eccfa1eaa5d7c4f994c23f48969b",
+    "url": "/portfolio/static/media/Ajay.ae91eccf.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e842fb7d8831b13b7113c9ee4db46cc2",
+    "url": "/portfolio/index.html"
+  }
+];
